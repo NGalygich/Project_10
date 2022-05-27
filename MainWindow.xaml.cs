55af2d5b32cf7f264274cf56bff2ffff6c9967ea
NGalygich <NGalygich@yandex.ru>
2022-05-27 22:32:51 +0300

    in developing

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Project_9;
+
+namespace Project_10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Project_9.Program.Main();
+            TextBlock textBlock1 = new TextBlock();
+            foreach (string el in Project_9.Program.messageList){
+                textBlock1.Text = el;
+            }
+            //string message1 = Project_9.Program.messageList;
+        }
+    }
+}
